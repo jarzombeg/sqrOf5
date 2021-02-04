fa9c1e71adaf135391a3f9e42cbf9434a8871118
jarzombeg <jarzombeg@gmail.com>
2021-02-04 18:11:26 +0000

    Initial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-                int resolution = sqr(5);
-                System.out.println(resolution);
-            }
+        // write your code here
+        int resolution = sqr(5);
+        System.out.println(resolution);
+    }
 
-            public static int sqr(int a) {
+    public static int sqr(int a) {
 
-                return a * a;
-            }
-        }
+        return a * a;
+    }
+}
