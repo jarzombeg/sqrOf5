1beee7fdb7a4e37b61a2fbf6b4fb6fe74dcb3649
jarzombeg <jarzombeg@gmail.com>
2021-02-04 14:36:45 +0000

    Initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+                int resolution = sqr(5);
+                System.out.println(resolution);
+            }
+
+            public static int sqr(int a) {
+
+                return a * a;
+            }
+        }
